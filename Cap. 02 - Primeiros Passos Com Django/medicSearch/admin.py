f2e1367559d17f5d9929f6f702c023a14e1c8dfd
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-14 16:00:30 -0300

    Capítulo 04: customizando a aparencia do painel do admin

--- Cap. 02 - Primeiros Passos Com Django/medicSearch/admin.py
+++ Cap. 02 - Primeiros Passos Com Django/medicSearch/admin.py
@@ -2,7 +2,37 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Profile)
+# Adds attr responsible for the apperance and behavior of the admin panel
+class ProfileAdmin(admin.ModelAdmin):
+    # Create a hierarquical filtering in the model Profile by creation date 
+    date_hierarchy = 'created_at' 
+    
+    # Select which attributes will be displayed
+    list_display = ('user', 'user__is_active', 'role', 'birthday',)
+    # Enable edition link for the following attributes 
+    lsit_display_links = ('user', 'role',)
+
+    # Cutomizes the representation of empty field
+    empty_value_display = 'Vazio'
+
+    # Creates a filter in the data based on the following attributes
+    list_filter = ('user__is_active',)
+
+    # Determines which field will be displayed in the form
+    fields = ('user', ('role',), 'image', 'birthday', 'specialities', 'addresses',)
+
+    # It is the opposite from fields, therefore removing the fields included in the tuple
+    exclude = ('favorites', 'created_at', 'updated_at')
+
+    # Makes the field read-only in the creation and update form
+    readonly_fields = ('user',)
+
+    # Enables the search by field in the admin profile list
+    search_fields = ('user__username',)
+
+
+# Note the argument ProfileAdmin bellow, is mandatory to include it in the register of the Profile model
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
