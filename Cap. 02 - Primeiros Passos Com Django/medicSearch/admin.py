2f35fa8ebbb819291d35a27fe99f1009c2212d0f
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-14 15:05:06 -0300

    Capítulo 03: adicionando as models ao arquivo admim.py

--- Cap. 02 - Primeiros Passos Com Django/medicSearch/admin.py
+++ Cap. 02 - Primeiros Passos Com Django/medicSearch/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Profile
+from .models import *
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Neighborhood)
+admin.site.register(Address)
+admin.site.register(DayWeek)
+admin.site.register(Rating)
+admin.site.register(Speciality)
