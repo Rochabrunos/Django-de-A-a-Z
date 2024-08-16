f122175955d84b125182a6decdbfb036fd345940
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-16 10:25:52 -0300

    Capítulo 07: usando o método render para renderizar o template home.html

--- Cap. 02 - Primeiros Passos Com Django/medicSearch/views/HomeView.py
+++ Cap. 02 - Primeiros Passos Com Django/medicSearch/views/HomeView.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_view(request):
-    return HttpResponse('<h1>Hello world!<h2>')
+    return render(request, template_name='home/home.html', status=200)
