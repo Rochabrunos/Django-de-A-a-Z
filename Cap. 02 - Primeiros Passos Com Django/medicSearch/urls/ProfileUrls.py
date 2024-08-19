4efe816217522370ad60e715c296bfdd5a574586
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-19 12:37:21 -0300

    Capítulo 09: criação da url para a view edit-profile

--- Cap. 02 - Primeiros Passos Com Django/medicSearch/urls/ProfileUrls.py
+++ Cap. 02 - Primeiros Passos Com Django/medicSearch/urls/ProfileUrls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from medicSearch.views.ProfileView import list_profile_view
+from medicSearch.views.ProfileView import list_profile_view, edit_profile
 
 urlpatterns = [
     path("", list_profile_view, name='profiles'),
     path("<int:id>/", list_profile_view, name='profile'),
+    path("edit/", edit_profile, name='edit-profile')
 ]
