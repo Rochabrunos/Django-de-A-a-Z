10f80836c0774bea87d91ad8fa14badbcb9697ab
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-19 14:20:41 -0300

    Capítulo 09: criando a tela de edição das informações do usuário (e.g. primeiro e último nome)

--- Cap. 02 - Primeiros Passos Com Django/medicSearch/forms/UserProfileForm.py
+++ Cap. 02 - Primeiros Passos Com Django/medicSearch/forms/UserProfileForm.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django import forms
-from medicSearch.models.Profile import Profile
+from medicSearch.models.Profile import Profile, User
 
 class UserProfileForm(ModelForm):
     def __init__(self, *args, **kwargs):
@@ -17,4 +17,16 @@ class UserProfileForm(ModelForm):
             'role': forms.Select(attrs={'class':'form-control'}),
             'birthday': forms.DateInput(attrs={'class': 'form-control', 'type':'date'}),
             'image': forms.FileInput(attrs={'class':'form-control'}),
+        }
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={'class':'form-control'}),
+            'email': forms.EmailInput(attrs={'class':'form-control'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
         }
--- Cap. 02 - Primeiros Passos Com Django/medicSearch/views/ProfileView.py
+++ Cap. 02 - Primeiros Passos Com Django/medicSearch/views/ProfileView.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from medicSearch.models import Profile
 from django.core.paginator import Paginator
-from medicSearch.forms.UserProfileForm import UserProfileForm
+from medicSearch.forms.UserProfileForm import UserProfileForm, UserForm
 
 def list_profile_view(request, id=None):
     if id is None and request.user.is_authenticated:
@@ -28,10 +28,25 @@ def list_profile_view(request, id=None):
 
 def edit_profile(request):
     profile = get_object_or_404(Profile, user=request.user)
-    profileForm = UserProfileForm(instance=profile)
+
+    emailUnused = True
+    if request.method == 'POST':
+        profileForm = UserProfileForm(request.POST, instance=profile)
+        userForm = UserForm(request.POST, instance=request.user)
+
+        verifyEmail = Profile.objects.filter(user__email=request.POST['email']).exclude(user__id=request.user.id).first()
+        emailUnused = verifyEmail is None
+    else:
+        profileForm = UserProfileForm(instance=profile)
+        userForm = UserForm(instance=request.user)
+
+    if profileForm.is_valid() and userForm.is_valid() and emailUnused:
+        profileForm.save()
+        userForm.save()
 
     context = {
         'profileForm': profileForm,
+        'userForm': userForm,
     }
 
     return render(request, template_name='user/profile.html', context=context, status=200)
