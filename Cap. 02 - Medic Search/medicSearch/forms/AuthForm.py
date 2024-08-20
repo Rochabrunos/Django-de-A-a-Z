de90b5386fedc37af7114f82649c32d3a698249e
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-20 13:16:46 -0300

    Capítulo 10: criação do formulário de registro de usuário

--- Cap. 02 - Medic Search/medicSearch/forms/AuthForm.py
+++ Cap. 02 - Medic Search/medicSearch/forms/AuthForm.py
@@ -3,4 +3,8 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'form-control'}), max_length=32)
-    
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'form-control'}), max_length=32)
--- Cap. 02 - Medic Search/medicSearchAdmin/settings/settings.py
+++ Cap. 02 - Medic Search/medicSearchAdmin/settings/settings.py
@@ -130,3 +130,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_URL = '/logout/'
+LOGOUT_REDIRECT_URL = '/login/'
