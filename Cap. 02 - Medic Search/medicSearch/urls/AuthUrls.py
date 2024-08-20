761ea8d22e5c1c06b377c1d27efbaf78f9e807e3
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-20 15:36:32 -0300

    Capítulo 11: adicionando a funcionalidade de logout

--- Cap. 02 - Medic Search/medicSearch/urls/AuthUrls.py
+++ Cap. 02 - Medic Search/medicSearch/urls/AuthUrls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from medicSearch.views.AuthView import login_view, register_view
+from medicSearch.views.AuthView import login_view, register_view, logout_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
+    path('logout/', logout_view, name='logout'),
 ]
